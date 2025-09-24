f8ed47b36257c97ac4c25ca4748b287aa245e96e
EoH <zien33195@gmail.com>
2025-09-24 11:10:33 +0900

    주석

--- Program/1Weeks6TeamMINI/Assets/Scripts/Board.cs
+++ Program/1Weeks6TeamMINI/Assets/Scripts/Board.cs
@@ -12,14 +12,14 @@ public class NewBehaviourScript : MonoBehaviour
     {
         int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
         arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
-
-        for (int i = 0; i < 16; i++)
+        //배치하려는 카드가 12개이므로 5f까지 남기기
+        for (int i = 0; i < 16; i++)// 12로 변경
         {
             GameObject go = Instantiate(card, this.transform);
 
             float x = (i % 4) * 1.4f - 2.1f;
             float y = (i / 4) * 1.4f - 3.0f;
-
+            //3개씩 4줄을 내려면 나누기를 4가 아닌 3으로 조정해야 함
             go.transform.position = new Vector2(x, y);
             go.GetComponent<card>().Setting(arr[i]);
         }
