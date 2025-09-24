bacd7c87a073255d112e745bfe1ffefe9e048d71
EoH <zien33195@gmail.com>
2025-09-24 12:25:50 +0900

    위치조정3

--- Program/1Weeks6TeamMINI/Assets/Scripts/Board.cs
+++ Program/1Weeks6TeamMINI/Assets/Scripts/Board.cs
@@ -18,7 +18,7 @@ public class NewBehaviourScript : MonoBehaviour
             GameObject go = Instantiate(card, this.transform);
 
             float x = (i % 3) * 1.6f - 1.6f;
-            float y = (i / 3) * 1.6f - 3.2f;
+            float y = (i / 3) * 1.8f - 3.2f;
             //3개씩 4줄을 내려면 나누기를 4가 아닌 3으로 조정해야 함
             go.transform.position = new Vector2(x, y);
             go.GetComponent<card>().Setting(arr[i]);
