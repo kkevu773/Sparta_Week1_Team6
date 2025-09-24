1c94f09c4afc76fc82a1f25a2c22a2aa7d0a8fa2
EoH <zien33195@gmail.com>
2025-09-24 12:16:22 +0900

    배치2

--- Program/1Weeks6TeamMINI/Assets/Scripts/Board.cs
+++ Program/1Weeks6TeamMINI/Assets/Scripts/Board.cs
@@ -17,8 +17,8 @@ public class NewBehaviourScript : MonoBehaviour
         {
             GameObject go = Instantiate(card, this.transform);
 
-            float x = (i % 3) * 1.6f - 2.1f;
-            float y = (i / 3) * 1.6f - 3.0f;
+            float x = (i % 3) * 1.6f - 1.6f;
+            float y = (i / 3) * 1.6f - 3.2f;
             //3개씩 4줄을 내려면 나누기를 4가 아닌 3으로 조정해야 함
             go.transform.position = new Vector2(x, y);
             go.GetComponent<card>().Setting(arr[i]);
